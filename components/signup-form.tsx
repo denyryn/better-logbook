cef8a6e5e636b6a6fed04c6e8e93df20637624c9
Deny Rian <denyryn@outlook.com>
2026-02-20 18:48:04 +0700

    added pages

---
+++ components/signup-form.tsx
@@ -0,0 +1,123 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema } from "@/schemas/sign-up";
+import { z } from "zod";
+import { api } from "@/lib/axios";
+
+type SignupFormData = z.infer<typeof signUpSchema>;
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isLoading },
+  } = useForm<SignupFormData>({
+    resolver: zodResolver(signUpSchema),
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data: SignupFormData) => {
+    api.post("api/auth/sign-up", data);
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Create your account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="name">Full Name</FieldLabel>
+                <Input id="name" {...register("name")} />
+                {errors.name && (
+                  <FieldDescription className="text-red-500">
+                    {errors.name.message}
+                  </FieldDescription>
+                )}
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input id="email" type="email" {...register("email")} />
+                {errors.email && (
+                  <FieldDescription className="text-red-500">
+                    {errors.email.message}
+                  </FieldDescription>
+                )}
+              </Field>
+              <Field>
+                <Field className="grid grid-cols-2 gap-4">
+                  <Field>
+                    <FieldLabel htmlFor="password">Password</FieldLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      {...register("password")}
+                    />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="confirm-password">
+                      Confirm Password
+                    </FieldLabel>
+                    <Input
+                      id="confirm-password"
+                      type="password"
+                      {...register("confirmPassword")}
+                    />
+                  </Field>
+
+                  {(errors.password || errors.confirmPassword) && (
+                    <FieldDescription className="text-red-500">
+                      {errors.password?.message ||
+                        errors.confirmPassword?.message}
+                    </FieldDescription>
+                  )}
+                </Field>
+                <FieldDescription>
+                  Must be at least 8 characters long.
+                </FieldDescription>
+              </Field>
+              <Field>
+                <Button type="submit" disabled={isSubmitting || isLoading}>
+                  {isSubmitting || isLoading ? "Creating..." : "Create Account"}
+                </Button>
+                <FieldDescription className="text-center">
+                  Already have an account? <a href="#">Sign in</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </FieldDescription>
+    </div>
+  );
+}
