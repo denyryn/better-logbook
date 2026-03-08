f3b50b0d4104031f48f850eaaa4f60d1368ea11b
Deny Rian <denyryn@outlook.com>
2026-03-08 18:41:00 +0700

    added ai usage verification on backend side

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -6,6 +6,8 @@ import { serverErrorResponse, serverSuccessResponse } from "@/lib/api.response";
 import { config } from "@/lib/config";
 import { AIProviderService } from "@/services/ai.provider.service";
 import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { TokenCalculator } from "@/lib/ai/token.calculator";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -16,6 +18,19 @@ export async function POST(request: NextRequest) {
   );
 
   try {
+    const session = await auth.api.getSession({ headers: request.headers });
+
+    const usages = await prisma.aiTokenUsage.findMany({
+      where: {userId: session?.user.id}
+    })
+
+    const tokenCalculator = new TokenCalculator(usages);
+    const remainingToken = tokenCalculator.calculateRemainingPercentage();
+
+    if (remainingToken === 0) {
+      return serverErrorResponse(undefined, "You are exceeded your AI usage limit.", StatusCodes.FORBIDDEN)
+    }
+
     const { prompt } = body
     const response = await ai.ask(prompt as string);
 
