d6a0bf7af1fc094e3aa6cb8861850ff92b86d218
Deny Rian <denyryn@outlook.com>
2026-03-09 01:59:58 +0700

    update global axios function to include error message

--- lib/axios.ts
+++ lib/axios.ts
@@ -6,3 +6,12 @@ export const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message = error.response?.data?.message || error.message || "An error occurred";
+    const inferencedError = new Error(message);
+    return Promise.reject(inferencedError);
+  },
+)
