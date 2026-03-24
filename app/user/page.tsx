c53ccce03feb10373c047e9a33dafd9fd159ea67
Deny Rian <denyryn@outlook.com>
2026-03-24 10:09:15 +0700

    added site header

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,19 @@
 "use client"
 
+import { SiteHeader } from "@/components/site-header";
 import Profile from "./_components/profile";
 
 export default function Page() {
 
   return (
-    <div className="container mx-auto space-y-6 px-4 py-10">
-      <Profile>
-        <Profile.Header />
-        <Profile.Content />
-      </Profile>
-    </div>
+    <>
+      <SiteHeader />
+      <div className="container mx-auto space-y-6 px-4 py-10">
+        <Profile>
+          <Profile.Header />
+          <Profile.Content />
+        </Profile>
+      </div>
+    </>
   );
 }
